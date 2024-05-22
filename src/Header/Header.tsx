778a2bef7058f0b196d0466e99c260885e989f48
Ostap <Ostapkriven13@gmail.com>
2024-05-22 19:47:42 +0300

    Add router

---
+++ src/Header/Header.tsx
@@ -0,0 +1,16 @@
+
+const Header = () => {
+  return (
+    <div>
+      <h1>Мій сайт</h1>
+      <nav>
+        <ul>
+          <li><a href="/">Домашня</a></li>
+          <li><a href="/portfolio">Портфоліо</a></li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/Portfolio/Portfolio.tsx
@@ -0,0 +1,11 @@
+
+const Portfolio = () => {
+  return (
+    <div>
+      <h2>Портфоліо</h2>
+      <p>Це сторінка з моїми роботами.</p>
+    </div>
+  );
+};
+
+export default Portfolio;
