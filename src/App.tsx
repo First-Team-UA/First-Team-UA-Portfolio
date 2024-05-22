890e78b3f9db5760cecfeb550022d670a6f4df81
Ostap <Ostapkriven13@gmail.com>
2024-05-22 20:32:40 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; 
+import { Route, Routes } from 'react-router-dom'; 
 import Header from './Header/Header';
 import Home from './Home/HomePage';
 import Portfolio from './Portfolio/Portfolio';
 
 const App = () => {
   return (
-    <Router>
       <div>
         <Header />
         <Routes>
@@ -14,7 +13,6 @@ const App = () => {
           <Route path="/portfolio" element={<Portfolio />} />
         </Routes>
       </div>
-    </Router>
   );
 };
 
