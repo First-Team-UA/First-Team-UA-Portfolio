2dfadad8384e2095ca101290734ae4fcef3f93a8
Ostap <Ostapkriven13@gmail.com>
2024-05-24 18:21:08 +0300

    Fix lng translate

--- src/SwitcherLng/Switcher.tsx
+++ src/SwitcherLng/Switcher.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
 import Select, { StylesConfig } from 'react-select';
 
@@ -10,8 +10,20 @@ interface OptionType {
 const LanguageSwitcher: React.FC = () => {
   const { i18n } = useTranslation();
 
+  // Зчитуємо обрану мову з localStorage
+  const storedLanguage = localStorage.getItem('i18nextLng');
+
+  // Встановлюємо англійську мову, якщо мова не була обрана раніше
+  useEffect(() => {
+    if (!storedLanguage) {
+      i18n.changeLanguage('en');
+      localStorage.setItem('i18nextLng', 'en');
+    }
+  }, [i18n, storedLanguage]);
+
   const changeLanguage = (lng: string) => {
     i18n.changeLanguage(lng);
+    localStorage.setItem('i18nextLng', lng);
   };
 
   const customStyles: StylesConfig<OptionType, false> = {
@@ -52,7 +64,7 @@ const LanguageSwitcher: React.FC = () => {
     { value: 'ua', label: 'Українська' },
   ];
 
-  const defaultLanguage = languageOptions.find((option) => option.value === 'en'); // Значення за замовчуванням - англійська
+  const defaultLanguage = languageOptions.find((option) => option.value === storedLanguage) || languageOptions[0];
 
   return (
     <div style={{ width: '200px' }}>
