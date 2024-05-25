2a9ed121c609cdfe62c77ff134349b0021db45d1
Ostap <Ostapkriven13@gmail.com>
2024-05-25 18:16:12 +0300

    add logo

--- src/Header/Header.tsx
+++ src/Header/Header.tsx
@@ -1,16 +1,23 @@
 import { NavLink } from "react-router-dom";
-import "./Header.css"
+import "./Header.css";
 import LanguageSwitcher from "../SwitcherLng/Switcher";
+import logo from "./logo.svg";
 
 const Header = () => {
   return (
     <header className="Header">
-      <NavLink to="/" className="Logo NavLink">Logo</NavLink>
-<nav>
-   <NavLink to="/" className="NavLink NavLink-home">Home</NavLink>
-         <NavLink to="/portfolio" className="NavLink NavLink-portfolio">Portfolio</NavLink>
-          </nav>
-<LanguageSwitcher></LanguageSwitcher>       
+      <NavLink to="/" className="Logo NavLink">
+        <img src={logo} alt="Logo" className="LogoImg" />
+      </NavLink>
+      <nav>
+        <NavLink to="/" className="NavLink NavLink-home">
+          Home
+        </NavLink>
+        <NavLink to="/portfolio" className="NavLink NavLink-portfolio">
+          Portfolio
+        </NavLink>
+      </nav>
+      <LanguageSwitcher />
     </header>
   );
 };
